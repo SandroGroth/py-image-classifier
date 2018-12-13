272c51b3df6aa2ce81217c76c41f0c65c49d7b6d
SandroGroth <sandro_groth@outlook.com>
2018-12-13 23:42:28 +0100

    IMG-IMPORT: Added main.py Script.

---
+++ main.py
@@ -0,0 +1,9 @@
+import logging
+from py_image_classifier import PyImageClassifier
+
+img_path = "/home/sandro/Documents/EAGLE_Data/WS201819_1st_Term/04GEOMB1_Digital_Image_Analysis/Steigerwald/03_raster/01_landsat/02_timescan"
+sample_path = "/home/sandro/Documents/EAGLE_Data/WS201819_1st_Term/04GEOMB1_Digital_Image_Analysis/Steigerwald/GIS_Analysis"
+model_path = "/home/sandro/Documents/EAGLE_Data/WS201819_1st_Term/04GEOMB1_Digital_Image_Analysis/Class_Py/model"
+out_path = "/home/sandro/Documents/EAGLE_Data/WS201819_1st_Term/04GEOMB1_Digital_Image_Analysis/Class_Py/output"
+
+py_image_classifier = PyImageClassifier(img_path, sample_path, model_path, out_path)
