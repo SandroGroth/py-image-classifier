06d7db18d84292da67ad2421bcf4d1c04b3520f8
SandroGroth <sandro_groth@outlook.com>
2018-12-13 22:52:37 +0100

    Built constructor.

---
+++ py-image-classifier/__init__.py
@@ -0,0 +1,9 @@
+
+class PyImageClassifier:
+
+    def __init__(self, img_path=None, sample_path=None, model_path=None, out_path=None):
+        
+        self.img_path = img_path
+        self.sample_path = sample_path
+        self.model_path = model_path
+        self.out_path = out_path
