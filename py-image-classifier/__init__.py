6af0d5ad71fc2ca6a0eed8658ba53c1f7507ede4
SandroGroth <sandro_groth@outlook.com>
2018-12-13 23:20:38 +0100

    IMG-IMPORT: Added setter methods.

--- py-image-classifier/__init__.py
+++ py-image-classifier/__init__.py
@@ -1,3 +1,5 @@
+import logging
+import os
 
 class PyImageClassifier:
 
@@ -7,3 +9,39 @@ class PyImageClassifier:
         self.sample_path = sample_path
         self.model_path = model_path
         self.out_path = out_path
+
+    def set_img_path(self, img_path):
+        
+        if os.path.exists(img_path):
+            self.img_path = img_path
+        else:
+            logging.error("Specified image path does not exist.")
+
+        return
+
+    def set_sample_path(self, sample_path):
+        
+        if os.path.exists(sample_path):
+            self.sample_path = sample_path
+        else:
+            logging.error("Specified training sample path does not exist.")
+
+        return
+
+    def set_model_path(self, model_path):
+        
+        if os.path.exists(model_path):
+            self.model_path = model_path
+        else:
+            logging.error("Specified model path does not exist.")
+
+        return
+
+    def set_out_path(self, out_path):
+        
+        if os.path.exists(out_path):
+            self.out_path = out_path
+        else:
+            logging.error("Specified classification output path does not exist.")
+
+        return
