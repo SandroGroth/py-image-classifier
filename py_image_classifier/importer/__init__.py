1d93eab154b8a2d861df3e9b2c771042ceac1b95
SandroGroth <sandro_groth@outlook.com>
2018-12-19 14:38:06 +0100

    IMG-IMPORT: Improved loggin in read_training_samples().

--- py_image_classifier/importer/__init__.py
+++ py_image_classifier/importer/__init__.py
@@ -38,42 +38,40 @@ class DataImporter:
                 logging.error('Failed to open training samples: {e}'.format(e=e))
                 return None
 
-            # getting dataset driver
+            # getting dshapefile infos
             driver = dataset.GetDriver()
-            logging.debug('Dataset driver is: {n}'.format(n=driver.name))
-
-            # checking layers in dataset
             layer_count = dataset.GetLayerCount()
-            logging.debug('Number of layers in shapefile: {n}'.format(n=layer_count))
-
-            # getting name of the 1. layer
             layer = dataset.GetLayerByIndex(0)
-            logging.debug('First layer name: {n}'.format(n=layer.GetName()))
-
-            # getting geometry info
             geometry = layer.GetGeomType()
             geometry_name = ogr.GeometryTypeToName(geometry)
-            logging.debug("Layer geometry: {geom}".format(geom=geometry_name))
-
-            # getting spatial reference info
             spatial_ref = layer.GetSpatialRef()
             proj4 = spatial_ref.ExportToProj4()
-            logging.debug('Spatial reference: {proj4}'.format(proj4=proj4))
-
-            # getting feature count
             feature_count = layer.GetFeatureCount()
-            logging.debug('Feature count: {n}'.format(n=feature_count))
-
-            # getting field count
             defn = layer.GetLayerDefn()
             field_count = defn.GetFieldCount()
-            logging.debug('Field count: {n}'.format(n=field_count))
-
-            # getting field names
+            fields_list = []
             for i in range(field_count):
                 field_defn = defn.GetFieldDefn(i)
-                logging.debug('\t{name} - {datatype}'.format(name=field_defn.GetName(),
-                                                            datatype=field_defn.GetTypeName()))
+                fields_list.append(field_defn.GetName())
+
+            logging.info("Sample data loaded: ")
+            logging.info("")
+            logging.info("+--------------------+--------------------------------------+")
+            logging.info("| Dataset Driver     | {d}".format(d=driver.name))
+            logging.info("+--------------------+--------------------------------------+")
+            logging.info("| Number of Layers   | {n}".format(n=layer_count))
+            logging.info("+--------------------+--------------------------------------+")
+            logging.info("| First Layer Name   | {f}".format(f=layer.GetName()))
+            logging.info("+--------------------+--------------------------------------+")
+            logging.info("| Layer geometry     | {l}".format(l=geometry_name))
+            logging.info("+--------------------+--------------------------------------+")
+            logging.info("| Spatial referene   | {p}".format(p=proj4))
+            logging.info("+--------------------+--------------------------------------+")
+            logging.info("| Feature count      | {c}".format(c=feature_count))
+            logging.info("+--------------------+--------------------------------------+")
+            logging.info("| Field names        | {f}".format(f=str(fields_list)))
+            logging.info("+--------------------+--------------------------------------+")
+            logging.info("")
 
             return dataset
 
